7ab36440e9ea85233d9da08902dd9930f268651a
hgxzoom <1254337121@qq.com>
2022-05-07 09:16:52 +0800

    feature:财务报表代码文件夹格式

---
+++ src/views/finance/basicData/yarn/index.vue
@@ -0,0 +1,21 @@
+<!-- 纱线数据维护 -->
+<template>
+  <div>纱线数据维护</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {},
+}
+</script>
+<style lang="scss" scoped></style>
