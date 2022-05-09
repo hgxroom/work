3bdbe55a5c8bc637f0b782210fcd91deaf0a29c5
hgxzoom <1254337121@qq.com>
2022-05-09 17:42:20 +0800

    fix:新增内部测试报表-科技

--- src/api/ordertestKJ/testreport.js
+++ src/api/ordertestKJ/testreport.js
@@ -6,7 +6,7 @@ import request, { download_JSON } from '@/utils/request'
 export function getExternalOrderList(params) {
   // let data = JSON.stringify(params) //转化JSON格式
   return request({
-    url: `hterp/external/getExternalOrderList`,
+    url: `hterp/external/getExternalOrderList2`,
     method: 'post',
     data: params,
   })
@@ -16,5 +16,5 @@ export function getExternalOrderList(params) {
  * 下载报表
  */
 export function externalExport(params) {
-  return download_JSON(`hterp/external/exportOrderList`, params, '内部测试报表-印染.xls')
+  return download_JSON(`hterp/external/exportOrderList`, params, '内部测试报表-科技.xls')
 }
