2db56beccf785c11e58af1297e88f423f82e924d
Peter <784708289@qq.com>
2022-05-16 17:52:58 +0800

    feat: 草稿状态可编辑

--- src/views/finance/report/addReport/utils.js
+++ src/views/finance/report/addReport/utils.js
@@ -2,15 +2,45 @@
  * 客户信息表单验证规则
  */
 export const formRules = {
-  //客户名称
+  // 客户名称
   customerName: [
     { required: true, message: '请输入客户名称', trigger: 'blur' },
-    { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'change' },
+    { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: ['change', 'blur'] },
   ],
-  //客户区域
-  referenceClothNo: {
+  // 最终客户
+  finalCustomerName: {
+    required: true,
+    message: '请输入最终客户',
+    trigger: 'change',
+  },
+  // 结算方式
+  settlementMethod: {
+    required: true,
+    message: '请输入结算方式',
+    trigger: 'change',
+  },
+}
+
+/**
+ * 添加产品
+ */
+export const formProductRules = {
+  // 布号
+  clothNo: {
     required: true,
     message: '请输入布号',
+    trigger: 'change',
+  },
+  // 下单重量
+  orderNum: {
+    required: true,
+    message: '请选择下单重量',
+    trigger: 'change',
+  },
+  // 颜色
+  colorName: {
+    required: true,
+    message: '请选择颜色',
     trigger: 'change',
   },
 }
